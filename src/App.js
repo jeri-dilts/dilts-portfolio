63dc1f6410602454de54a3ec3f3352c06d77d557
jeri <diltsjeri@gmail.com>
2021-02-09 17:41:31 -0500

    contact form working with emailjs

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import Layout from './components/Layout/Layout';
 // import Main from './screens/Main/Main';
 // import About from './screens/About/About';
 // import Portfolio from './screens/Portfolio/Portfolio';
-// import Contact from './screens/Contact/Contact';
+import Contact from './screens/Contact/Contact';
 
 function App() {
   return (
     <div className="App">
       <Layout>
         <Switch>
-          <Route path='/'>{/* <Main/> */}</Route>
           <Route path='/about'>{/* <About/> */}</Route>
           <Route path='/portfolio'>{/* <Portfolio/> */}</Route>
           <Route path='/resume'>{/* <Resume/> */}</Route>
-          <Route path='/contact'>{/* <Contact/> */}</Route>
+          <Route path='/contact'><Contact /></Route>
+          <Route path='/'>{/* <Main/> */}</Route>
         </Switch>
       </Layout>
     </div>
