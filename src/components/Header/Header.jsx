8c50be25f3648eb89254c67ded2b2db2e9467cdf
jeri <diltsjeri@gmail.com>
2021-02-09 20:40:10 -0500

    created download-able resume pdf link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import "./Header.css";
 import { Link } from "react-router-dom";
+import pdf from "../../img/Jeri_Dilts_Full_Stack_Developer_01282020.pdf"
 
 export default function Header() {
   return (
@@ -8,9 +9,11 @@ export default function Header() {
         <div className="right-nav">
             <Link to="about" className="link">About</Link>
             <Link to="portfolio" className="link">Portfolio</Link>
-            <Link to="resume" className="link">Resume</Link>
+            {/* <Link to="resume" className="link">Resume</Link> */}
+            <a href={pdf} className="link">Resume</a>
             <Link to="contact" className="link">Contact</Link>
         </div>
     </div>
   );
 }
+
