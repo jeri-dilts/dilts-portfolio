513d161c0480156a53b512b56e70c786a91279da
jeri <diltsjeri@gmail.com>
2021-02-10 22:02:30 -0500

    final push tnight. min reqs met

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import { useHistory, Switch, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
-// import Main from './screens/Main/Main';
-// import About from './screens/About/About';
+import Main from './screens/Main/Main';
+import About from './screens/About/About';
 import Portfolio from './screens/Porfolio/Portfolio';
 import Contact from './screens/Contact/Contact';
 
@@ -11,10 +11,10 @@ function App() {
     <div className="App">
       <Layout>
         <Switch>
-          <Route path='/about'>{/* <About/> */}</Route>
+          <Route path='/about'><About /></Route>
           <Route path='/portfolio'><Portfolio /></Route>
           <Route path='/contact'><Contact /></Route>
-          <Route path='/'>{/*<Main />*/}</Route>
+          <Route path='/'><Main /></Route>
         </Switch>
       </Layout>
     </div>
---
+++ src/screens/About/About.jsx
@@ -0,0 +1,10 @@
+import "./About.css";
+
+export default function About() {
+  return (
+    <div className="about-container">
+        <img className="portfolio_img"src="https://i.imgur.com/fjaB5Cc.jpg" alt="Professional Jeri Dilts Photo"></img>
+        <div className="about-paragraph">Hello! In my previous life I was a scientist, but now I'm a software engineer! Check out my GitHub as I build apps using ReactJS. In my previous career, I had a knack for finding creative solutions to difficult problems. I try to make my work fun and innovative as well as my approach to life. I thrive in challenging environments and am looking for a company just as ambitious as myself. www.linkedin.com/in/jeri-dilts</div>
+    </div>
+  );
+}
