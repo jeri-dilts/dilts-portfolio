6c60af42d716d5debef2bd78dbfd029f7da63091
jeri <diltsjeri@gmail.com>
2021-02-19 14:09:03 -0500

    added a description to my index.html and redirect resume pdf to open in a new tab

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,9 +9,18 @@ export default function Header() {
         <div className="right-nav">
             <Link to="about" className="link">About</Link>
             <Link to="portfolio" className="link">Portfolio</Link>
-            <a href={pdf} className="link">Resume</a>
+            <a href={pdf} className="link" target="_blank">Resume</a>
             <Link to="contact" className="link">Contact</Link>
         </div>
     </div>
   );
 }
+
+
+{/* <a
+href="https://drive.google.com/file/d/1PaMworqazqZXScpl3mw-xYM2phHTO6_M/view?usp=sharing"
+target="_blank"
+rel="noopener noreferrer"
+>
+Resume
+</a> */}
