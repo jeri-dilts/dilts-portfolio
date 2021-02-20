b4a2b3791a0ef014fa33d31021a6e47eceb18990
jeri <diltsjeri@gmail.com>
2021-02-19 22:41:41 -0500

    a couple minor edits. Also trying to get hamburger menu on portfolio screen to conditionally render

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,21 @@
 import "./Header.css";
+import Hamburger from 'hamburger-react'
 import { Link } from "react-router-dom";
-import pdf from "../../img/Jeri_Dilts_Full_Stack_Developer_02172021.pdf"
+import pdf from "../../img/Jeri_Dilts_Full_Stack_Developer_02172021.pdf";
+import { useState } from 'react';
 
 export default function Header() {
+
+  const [isOpen, setOpen] = useState(false) // hamburger menu toggle state
+
   return (
     <div className="header-container">
         <div className="name-link"><Link to="/ThanksForViewing!">Jeri Dilts</Link></div>
+      
+        <div className="hamburger-menu">
+          <Hamburger toggled={isOpen} toggle={setOpen} />
+        </div>
+
         <div className="right-nav">
             <Link to="about" className="link">About</Link>
             <Link to="portfolio" className="link">Portfolio</Link>
@@ -15,3 +25,6 @@ export default function Header() {
     </div>
   );
 }
+
+
+
