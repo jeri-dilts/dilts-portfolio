2c20ff28dcfc20ca51d6179ccb84d26eba40c516
jeri <diltsjeri@gmail.com>
2021-03-10 12:19:18 -0500

    updated another link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import "./Header.css";
 import Hamburger from 'hamburger-react'
 import { Link } from "react-router-dom";
-import pdf from "../../img/Jeri_Dilts_Full_Stack_Developer_02172021.pdf";
+import pdf from "../../img/Jeri_Dilts_Full_Stack_Developer_03_10_2021.pdf";
 import { useState, useEffect } from 'react';
 
 
