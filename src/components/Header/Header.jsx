8378b7be02650441f2479c594aa3c492d47fce10
jeri <diltsjeri@gmail.com>
2021-03-10 15:16:43 -0500

    establishing breakpoints

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import "./Header.css";
 import Hamburger from "hamburger-react";
+import Nav from "../Nav/Nav";
 import { Link } from "react-router-dom";
-import pdf from "../../img/Jeri_Dilts_Full_Stack_Developer_03_10_2021.pdf";
 import { useState, useEffect } from "react";
 
 export default function Header() {
@@ -20,45 +20,31 @@ export default function Header() {
     }
     window.addEventListener('resize', handleResize)
 
-    // cleaning up event listener so we are only handling a single event at a time
+    // cleaning up event listener (avoid binding)
     return _ => {
       window.removeEventListener('resize', handleResize)
     }
   })
   return (
+
     <div className="header-container">
+
+
       <div className="name-link">
         <Link to="/">Jeri Dilts</Link>
       </div>
 
 
-
       <div className="hamburger-icon">
-        {dimensions.width < 416 && <Hamburger />}
+        {dimensions.width < 1000 && <Hamburger />}
       </div>
 
 
-      {/* {dimensions.width < 416 && <Hamburger toggled={isOpen} toggle={setOpen} />} */}
-      {/* {dimensions.width > 415 && } */}
-
-      {/* <div className="hamburger-menu">
-          <Hamburger toggled={isOpen} toggle={setOpen} />
-        </div> */}
-
-      <div className="right-nav">
-        <Link to="about" className="link">
-          About
-        </Link>
-        <Link to="portfolio" className="link">
-          Portfolio
-        </Link>
-        <a href={pdf} className="link" target="_blank">
-          Resume
-        </a>
-        <Link to="contact" className="link">
-          Contact
-        </Link>
+      <div className="regular-nav">
+        {dimensions.width > 1000 && <Nav />}
       </div>
+
+
     </div>
   )
 }
@@ -78,3 +64,10 @@ export default function Header() {
 // When window size get larger, hamburger menu disappears and nav appears
 // Position hamburger menu in the right location
 // On click (toggle) hamburger menu gives nav options
+
+      // {/* {dimensions.width < 416 && <Hamburger toggled={isOpen} toggle={setOpen} />} */}
+      // {/* {dimensions.width > 415 && } */}
+
+      // {/* <div className="hamburger-menu">
+      //     <Hamburger toggled={isOpen} toggle={setOpen} />
+      //   </div> */}
